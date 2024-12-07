be9d9b893a5df9c0535af0c27f29f76286e0eeae
Galj305 <galj@unm.edu>
2024-12-07 06:47:02 +0500

    Update project_app_code.py

--- project_app_code.py
+++ project_app_code.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1f5egxzOWinxMyV54XTifj49uvrargK4r
 """
 
-! pip install streamlit
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
